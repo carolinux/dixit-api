fd57a61e1f5eb584ba4127792b143abc2ca09d8b
Eleni Mandilara <madelinegr@gmail.com>
2020-12-03 22:12:40 +0100

    Remove unused routes

--- server.py
+++ server.py
@@ -4,14 +4,6 @@ import random
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
-def index():
-  return 'Server Works!'
-
-@app.route('/greet')
-def say_hello():
-  return 'Hello from Server'
-
 cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 players = ['Eleni', 'George', 'Theodore']
 
