62ede30a76eec0bf16e57a65cc0f4554f8bceafd
Eleni Mandilara <madelinegr@gmail.com>
2020-12-04 20:32:32 +0100

    Add methods to return players & cards, and calculate next player

--- server.py
+++ server.py
@@ -1,33 +1,89 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+# from flask.ext.socketio import SocketIO, emit
 import random
+# from score import updateScore
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+# socketio = SocketIO(app)
 app.config["DEBUG"] = True
 
-cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-players = ['Eleni', 'George', 'Theodore']
+# Game constants
+maxPlayersCount = 6
+cardsPerPlayerCount = 6
 
-# Shuffles cards & returns their distribution per player
+players = [
+  { 'name': 'Eleni', 'hasTurn': False  },
+  { 'name': 'George', 'hasTurn': False },
+  { 'name': 'Theodore', 'hasTurn': False }
+]
+
+cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+random.shuffle(cards)
+
+for player in players:
+  player['cards'] = cards[0:cardsPerPlayerCount]
+  del cards[:cardsPerPlayerCount]
+
+# @socketio.on('my event')                          # Decorator to catch an event called "my event":
+# def test_message(message):                        # test_message() is the event callback function.
+#   emit('my response', {'data': 'got it!'})        # Trigger a new event called "my response" 
+
+
+# Return players
+@app.route('/players', methods=['GET'])
+def return_players():
+  return jsonify(players)
+
+# Add new player
+@app.route('/players', methods=['POST'])
+def add_player():
+  player = { 'name': request.json['player'], 'hasTurn': False }
+  if len(players)<maxPlayersCount:
+    players.append(player)
+  return jsonify(players)
+
+# Add new player
+@app.route('/start', methods=['POST'])
+def start_game():
+  random.shuffle(players)
+  players[0]['hasTurn'] = True
+  return jsonify(players)
+
+# Shuffle cards & return their distribution per player
 @app.route('/initialCardsDistribution')
 def distribute_cards():
-  random.shuffle(cards)
-  cardsPerPlayer = {}
+  return players
 
+# Return cards of a player
+@app.route('/cards')
+def return_cards_per_player():
+  cards = []
+  playerName = request.args.get('player')
   for player in players:
-    cardsPerPlayer[player] = cards[0:6]
-    del cards[:6]
-  return cardsPerPlayer
+    if player['name'] == playerName:
+      cards = player['cards']   
+    
+  return jsonify(cards)
 
-# Returns true if it is this player's turn, false otherwise
+# Return true if it is this player's turn, false otherwise
 @app.route('/hasTurn', methods=['GET'])
 def return_player_turn():
   # TODO: Implement logic
   return jsonify({'hasTurn': True})
 
-# Calculates what should happen at the end of each round
-# & returns next player (if the game has not finished)
+# Calculate what should happen at the end of each round
+# & return the next player (if the game has not finished)
 @app.route('/roundCompleted', methods=['POST'])
 def complete_round():
+  # print(updateScore([]))
+ 
+  # Calculate next player
+  players[0]['hasTurn'] = False
+  players.append(players[0])
+  players.pop(0)
+  players[0]['hasTurn'] = True
+  return jsonify(players)
   # TODO:
   # 1. Calculate & update scores
   # 2. Check if maximum score was reached
